6384f9a1fde8741b5d56cb2a2a843bdcbf87a081
aamirpinger <aamirpinger@yahoo.com>
2021-01-11 00:27:59 +0500

    ddb

--- lib/step01_hello_lambda-stack.ts
+++ lib/step01_hello_lambda-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from "@aws-cdk/core";
-import * as lambda from "@aws-cdk/aws-lambda";
-import * as apigw from "@aws-cdk/aws-apigateway";
+import * as ddb from '@aws-cdk/aws-dynamodb';
 
 export class Step01HelloLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
